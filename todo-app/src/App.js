03023d67b553db9f66c09d4c3851561f60cf8789
kimkyutae <rbxo9085@ulalalab.com>
2022-08-31 00:24:23 +0900

    [react-dev] useState 사용 및 props를 통한 Data전달

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
 
 const App = () => {
-  return <div>Todo App만들기 </div>;
+  /* state */
+  const [todos, setTodos] = useState([
+    { id: 1, text: '할일1', checked: true },
+    { id: 2, text: '할일2', checked: true },
+    { id: 3, text: '할일3', checked: false },
+  ]);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert></TodoInsert>
+      <TodoList todos={todos}></TodoList>
+    </TodoTemplate>
+  );
 };
 
 export default App;
