92c79c5fa164300d6e281fc8c9586eb23d20565a
kimkyutae <rbxo9085@ulalalab.com>
2022-08-31 07:48:55 +0900

    [react-dev] TodoInsert onSubmit 이벤트 설정

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -2,20 +2,29 @@ import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   /* state */
   const [value, setValue] = useState('');
 
   /**
    * 함수를 재사용하기 위해 useCallback 사용
+   * 할 일 입력 input Event
    */
   const onChange = useCallback((e) => {
-    const { value } = e.target || {};
-    setValue(value);
+    setValue(e.target.value);
   }, []);
 
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      onInsert(value);
+      setValue('');
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className='TodoInsert'>
+    <form className='TodoInsert' onSubmit={onSubmit}>
       <input
         placeholder='할 일을 입력하세요.'
         value={value}
