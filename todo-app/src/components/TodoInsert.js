c07e1abd0f0545da975b134e61f5883437b2f7dd
kimkyutae <rbxo9085@ulalalab.com>
2022-08-31 07:26:14 +0900

    [react-dev] TodoInsert value 상태관리

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+  /* state */
+  const [value, setValue] = useState('');
+
+  /**
+   * 함수를 재사용하기 위해 useCallback 사용
+   */
+  const onChange = useCallback((e) => {
+    const { value } = e.target || {};
+    setValue(value);
+  }, []);
+
   return (
     <form className='TodoInsert'>
-      <input placeholder='할 일을 입력하세요.'></input>
+      <input
+        placeholder='할 일을 입력하세요.'
+        value={value}
+        onChange={onChange}
+      ></input>
       <button type='submit'>
         <MdAdd></MdAdd>
       </button>
