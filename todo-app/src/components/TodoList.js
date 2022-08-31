1c5c1f2d5173a1665069fd2db62ae2260ae639d4
kimkyutae <rbxo9085@ulalalab.com>
2022-08-31 23:27:02 +0900

    [react-dev] TodoList 삭재이벤트 Props

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './TodoList.scss';
 import TodoListItem from './TodoListItem';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
   return (
     <div className='TodoList'>
       {todos?.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id}></TodoListItem>
+        <TodoListItem
+          todo={todo}
+          key={todo.id}
+          onRemove={onRemove}
+        ></TodoListItem>
       ))}
     </div>
   );
